149d001ce29150d2c72511f279140bf52bffbfe6
Viv-Hegde <98569805+Viv-Hegde@users.noreply.github.com>
2022-04-23 23:42:25 -0700

    Add files via upload

---
+++ Driver.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+public class Driver {
+    public static void main(String [] args){
+        
+    Scanner keyboard = new Scanner (System.in);
+    String plainText;
+    int userKey;
+    System.out.print("Enter a word/phrase you want to encrypt(CAPS): ");
+    plainText = keyboard.next();
+    System.out.print("Enter your secret cipher key:");
+    userKey = keyboard.nextInt();
+    System.out.println("\n*********************ENCRYPTING*********************");
+
+    Char_encryption c = new Char_encryption(userKey);
+    System.out.println("Your ciper text is : " + c.encrypt(plainText)+"\n");
+    keyboard.close();
+
+    
+    }
+    
+}
